d8f340b22507a49b567aafb0baa48c6d38c1de4c
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-30 10:21:19 -0700

    static exercise completed

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double tempFahrenheit = TempConverter.CelsiusToFarenheit(50);
+            double tempCelsius = TempConverter.FahrenheitToCelsius(50);
+            
+            Console.WriteLine($"Temp Fahrenheit: {tempFahrenheit}");
+            Console.WriteLine($"Temp Celsius: {tempCelsius}");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,12 @@
+﻿public static class TempConverter
+{
+    public static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double CelsiusToFarenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+}
